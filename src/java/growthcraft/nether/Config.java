cd655b36e779f4f614f406684fb4e0073b2b54ca
Corey Powell <mistdragon100@gmail.com>
2015-10-15 09:56:50 -0500

    nether: Placeholder fire brandy and malice cider colors

--- src/java/growthcraft/nether/Config.java
+++ src/java/growthcraft/nether/Config.java
@@ -8,8 +8,8 @@ public class Config extends ConfigBase
 	public int minPepperPicked = 1;
 	public int maxPepperPicked = 8;
 
-	public final int fireBrandyColor = 0xFFFFFF;
-	public final int maliceCiderColor = 0xFFFFFF;
+	public final int fireBrandyColor = 0xD43427;
+	public final int maliceCiderColor = 0xA99F6E;
 
 	protected void loadConfig()
 	{
