c5dd294ffbceb68e5c93b22d8480ea6b44ce3791
Corey Powell <mistdragon100@gmail.com>
2015-10-16 09:49:24 -0500

    nether: Added missing import for BlockNetherPaddy

--- src/java/growthcraft/nether/common/block/BlockNetherPaddy.java
+++ src/java/growthcraft/nether/common/block/BlockNetherPaddy.java
@@ -14,6 +14,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.util.IIcon;
