99c3ecbb34ae1851e523e3a1793f85678f5feb50
Corey Powell <mistdragon100@gmail.com>
2015-10-18 22:57:45 -0500

    grapes: Removed unused imports

--- src/java/growthcraft/grapes/block/BlockGrapeBlock.java
+++ src/java/growthcraft/grapes/block/BlockGrapeBlock.java
@@ -9,11 +9,9 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.util.AxisAlignedBB;
-import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
 public class BlockGrapeBlock extends Block
