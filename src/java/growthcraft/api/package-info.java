ee5a3cb8113dcbe3a8eb74ac325e201f99e5099e
Corey Powell <mistdragon100@gmail.com>
2015-11-14 10:46:42 -0500

    api: Bumped api version to 2.4
    It was still 2.0 for some odd reason.

--- src/java/growthcraft/api/package-info.java
+++ src/java/growthcraft/api/package-info.java
@@ -1,3 +1,3 @@
-@API(apiVersion="2.0", owner="Growthcraft", provides="GrowthcraftAPI|Core")
+@API(apiVersion="2.4", owner="Growthcraft", provides="GrowthcraftAPI|Core")
 package growthcraft.api;
 import cpw.mods.fml.common.API;
