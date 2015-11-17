53a0608b814be2e12667b2c5c0b5def8ee49fd4f
Corey Powell <mistdragon100@gmail.com>
2015-11-16 19:27:49 -0500

    core: Ported AE2 tile event system

---
+++ src/java/growthcraft/core/common/tileentity/event/EventFunction.java
@@ -0,0 +1,86 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 IceDragon200
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package growthcraft.core.common.tileentity.event;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import javax.annotation.Nonnull;
+
+import io.netty.buffer.ByteBuf;
+
+import growthcraft.core.common.tileentity.GrcBaseTile;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class EventFunction
+{
+	private Method method;
+
+	public EventFunction(@Nonnull Method m)
+	{
+		this.method = m;
+	}
+
+	public void writeToStream(GrcBaseTile tile, ByteBuf data)
+	{
+		try
+		{
+			this.method.invoke(tile, data);
+		}
+		catch (IllegalAccessException e)
+		{
+			throw new IllegalStateException(e);
+		}
+		catch (IllegalArgumentException e)
+		{
+			throw new IllegalStateException(e);
+		}
+		catch (InvocationTargetException e)
+		{
+			throw new IllegalStateException(e);
+		}
+	}
+
+	@SideOnly (Side.CLIENT)
+	public boolean readFromStream(GrcBaseTile tile, ByteBuf data)
+	{
+		try
+		{
+			return (Boolean)this.method.invoke(tile, data);
+		}
+		catch (IllegalAccessException e)
+		{
+			throw new IllegalStateException(e);
+		}
+		catch (IllegalArgumentException e)
+		{
+			throw new IllegalStateException(e);
+		}
+		catch (InvocationTargetException e)
+		{
+			throw new IllegalStateException(e);
+		}
+	}
+}
