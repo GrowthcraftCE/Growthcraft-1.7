3fd312f0e664a0665badf8802e00d35aba52d9ac
Corey Powell <mistdragon100@gmail.com>
2015-11-21 10:24:24 -0500

    core: Completed AuxFX listing

--- src/java/growthcraft/core/util/AuxFX.java
+++ src/java/growthcraft/core/util/AuxFX.java
@@ -2,7 +2,36 @@ package growthcraft.core.util;
 
 public class AuxFX
 {
+	public static final int CLICK1 = 1000;
+	public static final int CLICK2 = 1001;
+	public static final int BOW = 1002;
+	public static final int DOOR = 1003;
+	public static final int FIZZ = 1004;
+	public static final int RECORD = 1005;
+	public static final int UNUSUED0_ = 1006;
+	public static final int GHAST_CHARGE = 1007;
+	public static final int GHAST_FIREBALL = 1008;
+	public static final int GHAST_FIREBALL2 = 1009;
+	public static final int ZOMBIE_WOOD = 1010;
+	public static final int ZOMBIE_METAL = 1011;
+	public static final int ZOMBIE_WOOD_BREAK = 1012;
+	public static final int WITHER_SHOOT = 1014;
+	public static final int BAT_TAKEOFF = 1015;
+	public static final int ZOMBIE_INFECT = 1016;
+	public static final int ZOMBIE_UNFECT = 1017;
+	public static final int UNUSUED1_ = 1018;
+	public static final int UNUSUED2_ = 1019;
+	public static final int ANVIL_BREAK = 1020;
+	public static final int ANVIL_USE = 1021;
+	public static final int ANVIL_LAND = 1022;
+
+	public static final int SMOKE = 2000;
+	public static final int BREAK = 2001;
+	public static final int SPELL = 2002;
+	public static final int PORTAL = 2003;
+	public static final int SMOKE_FIRE = 2004;
 	public static final int BONEMEAL = 2005;
+	public static final int BLOCKDUST = 2006;
 
 	private AuxFX() {}
 }
