40a2aaa49ac1570c2f36f07d3d81bbc7ea953779
Corey Powell <mistdragon100@gmail.com>
2015-11-26 12:12:06 -0500

    dairy: Register booze

--- src/java/growthcraft/dairy/init/GrcDairyBooze.java
+++ src/java/growthcraft/dairy/init/GrcDairyBooze.java
@@ -9,10 +9,15 @@ import growthcraft.cellar.common.definition.BlockBoozeDefinition;
 import growthcraft.cellar.common.definition.ItemBucketBoozeDefinition;
 import growthcraft.cellar.common.item.ItemBoozeBottle;
 import growthcraft.cellar.util.BoozeRegistryHelper;
+import growthcraft.core.common.definition.ItemDefinition;
 import growthcraft.core.common.GrcModuleBase;
+import growthcraft.dairy.GrowthCraftDairy;
+
+import net.minecraftforge.fluids.FluidStack;
 
 public class GrcDairyBooze extends GrcModuleBase
 {
+	private static final String milkBasename = "grcdairy.milk";
 	// not an actual Booze, but we use the class since it has the ability to set color
 	public Booze[] milkBooze = new Booze[5];
 	public BlockBoozeDefinition[] milkBoozeBlocks = new BlockBoozeDefinition[milkBooze.length];
@@ -23,7 +28,7 @@ public class GrcDairyBooze extends GrcModuleBase
 	public void preInit()
 	{
 		this.milk = new ItemDefinition(new ItemBoozeBottle(5, -0.6F, milkBooze));
-		BoozeRegistryHelper.initializeBooze(milkBooze, milkBoozeBlocks, milkBoozeBuckets, "grcdairy.milk", GrowthCraftDairy.getConfig().milkColor);
+		BoozeRegistryHelper.initializeBooze(milkBooze, milkBoozeBlocks, milkBoozeBuckets, milkBasename, GrowthCraftDairy.getConfig().milkColor);
 	}
 
 	private void registerFermentations()
@@ -52,6 +57,7 @@ public class GrcDairyBooze extends GrcModuleBase
 	@Override
 	public void register()
 	{
+		BoozeRegistryHelper.registerBooze(milkBooze, milkBoozeBlocks, milkBoozeBuckets, milk, milkBasename, null);
 		registerFermentations();
 	}
 }
