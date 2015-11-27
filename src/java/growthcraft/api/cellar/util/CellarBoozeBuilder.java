a8f034e0dbe7315a5c8cc6afbf0ffd077e557084
Corey Powell <mistdragon100@gmail.com>
2015-11-27 14:44:34 -0500

    api/cellar/util: Added CellarBoozeBuilder
    Yes, this got so complicated I needed a Builder.

---
+++ src/java/growthcraft/api/cellar/util/CellarBoozeBuilder.java
@@ -0,0 +1,95 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 IceDragon200
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package growthcraft.api.cellar.util;
+
+import javax.annotation.Nonnull;
+
+import growthcraft.api.cellar.booze.BoozeEffect;
+import growthcraft.api.cellar.booze.BoozeTag;
+import growthcraft.api.cellar.CellarRegistry;
+import growthcraft.api.cellar.common.Residue;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidStack;
+
+/**
+ * If you find yourself making some seriously gnarly spaghetti code, this may
+ * save you.
+ */
+public class CellarBoozeBuilder
+{
+	private Fluid fluid;
+
+	public CellarBoozeBuilder(Fluid f)
+	{
+		this.fluid = f;
+	}
+
+	public CellarBoozeBuilder tags(BoozeTag... tags)
+	{
+		CellarRegistry.instance().booze().addTags(fluid, tags);
+		return this;
+	}
+
+	public CellarBoozeBuilder brewsTo(@Nonnull FluidStack result, @Nonnull ItemStack stack, int time, @Nonnull Residue residue)
+	{
+		CellarRegistry.instance().brewing().addBrewing(new FluidStack(fluid, result.amount), stack, result, time, residue);
+		return this;
+	}
+
+	public CellarBoozeBuilder brewsFrom(@Nonnull FluidStack src, @Nonnull ItemStack stack, int time, @Nonnull Residue residue)
+	{
+		CellarRegistry.instance().brewing().addBrewing(src, stack, new FluidStack(fluid, src.amount), time, residue);
+		return this;
+	}
+
+	public CellarBoozeBuilder fermentsTo(@Nonnull FluidStack result, @Nonnull ItemStack stack, int time)
+	{
+		CellarRegistry.instance().fermenting().addFermentation(result, new FluidStack(fluid, result.amount), stack, time);
+		return this;
+	}
+
+	public CellarBoozeBuilder fermentsFrom(@Nonnull FluidStack src, @Nonnull ItemStack stack, int time)
+	{
+		CellarRegistry.instance().fermenting().addFermentation(new FluidStack(fluid, src.amount), src, stack, time);
+		return this;
+	}
+
+	public CellarBoozeBuilder pressesFrom(@Nonnull ItemStack stack, int time, int amount, @Nonnull Residue residue)
+	{
+		CellarRegistry.instance().pressing().addPressing(stack.getItem(), stack.getItemDamage(), fluid, time, amount, residue);
+		return this;
+	}
+
+	public BoozeEffect getEffect()
+	{
+		return CellarRegistry.instance().booze().getEffect(fluid);
+	}
+
+	public static CellarBoozeBuilder create(Fluid f)
+	{
+		return new CellarBoozeBuilder(f);
+	}
+}
