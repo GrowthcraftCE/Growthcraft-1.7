8270c186b55b7b4219d678395c0cd473ced1d116
Corey Powell <mistdragon100@gmail.com>
2016-01-29 22:19:38 -0500

    core/util: Added roundedBlocks and closestPowerOf2
    Can't remember why I need these...

--- src/java/growthcraft/core/util/NumUtils.java
+++ src/java/growthcraft/core/util/NumUtils.java
@@ -27,6 +27,23 @@ public class NumUtils
 {
 	private NumUtils() {}
 
+	public static int roundedBlocks(int num, int blocksize)
+	{
+		int n = num / blocksize;
+		if (n < num) n += blocksize;
+		return n;
+	}
+
+	public static int closestPowerOf2(int num)
+	{
+		int start = 1;
+		while (start < num)
+		{
+			start *= 2;
+		}
+		return start;
+	}
+
 	public static boolean between(int num, int bot, int top)
 	{
 		return num >= bot && num <= top;
