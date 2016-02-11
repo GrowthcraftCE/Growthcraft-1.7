e61708189c0178331baff3a7afceb0b682b2b096
Corey Powell <mistdragon100@gmail.com>
2016-02-11 15:59:22 -0500

    api: Bumped API versions to 2.5

--- src/java/growthcraft/api/bees/package-info.java
+++ src/java/growthcraft/api/bees/package-info.java
@@ -1,4 +1,4 @@
-@API(apiVersion="2.4", owner="GrowthcraftAPI|Core", provides="GrowthcraftAPI|Bees")
+@API(apiVersion="2.5", owner="GrowthcraftAPI|Core", provides="GrowthcraftAPI|Bees")
 package growthcraft.api.bees;
 
 import cpw.mods.fml.common.API;
