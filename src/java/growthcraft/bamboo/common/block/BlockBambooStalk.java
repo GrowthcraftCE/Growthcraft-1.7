ddad0737a2c378e910968fcd1e7a3d030fb80769
Corey Powell <mistdragon100@gmail.com>
2016-02-11 15:09:50 -0500

    bamboo: Allow torches to be placed on Bamboo Stalks

--- src/java/growthcraft/bamboo/common/block/BlockBambooStalk.java
+++ src/java/growthcraft/bamboo/common/block/BlockBambooStalk.java
@@ -44,6 +44,12 @@ public class BlockBambooStalk extends Block
 		this.setBlockName("grc.bambooStalk");
 	}
 
+	@Override
+	public boolean canPlaceTorchOnTop(World world, int x, int y, int z)
+	{
+		return true;
+	}
+
 	/************
 	 * TICK
 	 ************/
