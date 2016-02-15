49c4e54706a6796daa0a0be8e1e8108bfeda8f0a
Corey Powell <mistdragon100@gmail.com>
2016-02-14 22:01:09 -0500

    api/core/util: Added TaggedFluidStacks
    Just like the OreItemStacks, but for fluids, and uses Growthcraft's FluidDictionary

---
+++ src/java/growthcraft/api/core/util/TaggedFluidStacks.java
@@ -0,0 +1,70 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 IceDragon200
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package growthcraft.api.core.util;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import growthcraft.api.core.CoreRegistry;
+import growthcraft.api.core.definition.IMultiFluidStacks;
+import growthcraft.api.core.fluids.FluidTag;
+
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidStack;
+
+public class TaggedFluidStacks implements IMultiFluidStacks
+{
+	public int amount;
+	private List<String> tags;
+
+	public TaggedFluidStacks(int amt, String... ptags)
+	{
+		this.amount = amt;
+		this.tags = Arrays.asList(ptags);
+	}
+
+	public List<String> getTags()
+	{
+		return tags;
+	}
+
+	public Collection<Fluid> getFluids()
+	{
+		final List<FluidTag> fluidTags = CoreRegistry.instance().fluidTags().expandTagNames(tags);
+		return CoreRegistry.instance().fluidDictionary().getFluidsByTags(fluidTags);
+	}
+
+	public List<FluidStack> getFluidStacks()
+	{
+		final Collection<Fluid> fluids = getFluids();
+		final List<FluidStack> result = new ArrayList<FluidStack>();
+		for (Fluid fluid : fluids)
+		{
+			result.add(new FluidStack(fluid, amount));
+		}
+		return result;
+	}
+}
