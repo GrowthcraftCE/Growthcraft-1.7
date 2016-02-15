70c29e10ae63819143bf871962ec6794e6d344a8
Corey Powell <mistdragon100@gmail.com>
2016-02-14 21:58:55 -0500

    api/core/util: Updated ItemKey to support NBT data

--- src/java/growthcraft/api/core/util/ItemKey.java
+++ src/java/growthcraft/api/core/util/ItemKey.java
@@ -24,52 +24,84 @@
 package growthcraft.api.core.util;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import growthcraft.api.core.definition.IItemStackFactory;
+import growthcraft.api.core.nbt.NBTHelper;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.oredict.OreDictionary;
 
 /**
  * As the name implies, this class is used in place of a List for Item keys
  */
-public class ItemKey extends HashKey
+public class ItemKey extends HashKey implements IItemStackFactory
 {
 	public static final int WILDCARD_VALUE = OreDictionary.WILDCARD_VALUE;
 
 	public final Item item;
 	public final int meta;
+	public final NBTTagCompound compoundTag;
 
-	public ItemKey(@Nonnull Item iitem, int imeta)
+	public ItemKey(@Nonnull Item pitem, int pmeta, @Nullable NBTTagCompound tag)
 	{
 		super();
-		this.item = iitem;
-		this.meta = imeta;
+		this.item = pitem;
+		this.meta = pmeta;
+		this.compoundTag = tag;
 		generateHashCode();
 	}
 
-	public ItemKey(@Nonnull Block block, int imeta)
+	public ItemKey(@Nonnull Item pitem, int pmeta)
+	{
+		this(pitem, pmeta, null);
+	}
+
+	public ItemKey(@Nonnull Block block, int pmeta, @Nullable NBTTagCompound tag)
 	{
 		super();
-		final Item iitem = Item.getItemFromBlock(block);
-		if (iitem == null)
+		final Item pitem = Item.getItemFromBlock(block);
+		if (pitem == null)
 		{
-			throw new IllegalArgumentException("Invalid Block given for ItemKey (block=" + block + " meta=" + imeta + ")");
+			throw new IllegalArgumentException("Invalid Block given for ItemKey (block=" + block + " meta=" + pmeta + ")");
 		}
-		this.item = iitem;
-		this.meta = imeta;
+		this.item = pitem;
+		this.meta = pmeta;
+		this.compoundTag = tag;
 		generateHashCode();
+	}
+
+	public ItemKey(@Nonnull Block block, int pmeta)
+	{
+		this(block, pmeta, null);
 	}
 
 	public ItemKey(@Nonnull ItemStack stack)
 	{
-		this(stack.getItem(), stack.getItemDamage());
+		this(stack.getItem(), stack.getItemDamage(), stack.getTagCompound());
 	}
 
 	public void generateHashCode()
 	{
 		this.hash = item.hashCode();
 		this.hash = 31 * hash + meta;
+		this.hash = 31 * hash + (compoundTag != null ? compoundTag.hashCode() : 0);
+	}
+
+	@Override
+	public ItemStack asStack(int size)
+	{
+		final ItemStack result = new ItemStack(item, size, meta);
+		result.setTagCompound(NBTHelper.copyCompoundTag(compoundTag));
+		return result;
+	}
+
+	@Override
+	public ItemStack asStack()
+	{
+		return asStack(1);
 	}
 }
