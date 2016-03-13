839d910e49d956ef96ecb02cdd17c6b400ba9265
Corey Powell <mistdragon100@gmail.com>
2016-03-12 21:18:47 -0500

    apples,core: Started integration work on MFR

---
+++ src/java/growthcraft/core/integration/mfr/AbstractFactoryHarvestable.java
@@ -0,0 +1,82 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 IceDragon200
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package growthcraft.core.integration.mfr;
+
+import java.util.Map;
+import java.util.List;
+import java.util.Random;
+
+import growthcraft.core.integration.MFRModuleBase;
+
+import powercrystals.minefactoryreloaded.api.HarvestType;
+import powercrystals.minefactoryreloaded.api.IFactoryHarvestable;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+@Optional.Interface(iface="powercrystals.minefactoryreloaded.api.IFactoryHarvestable", modid=MFRModuleBase.MOD_ID)
+public abstract class AbstractFactoryHarvestable implements IFactoryHarvestable
+{
+	@Override
+	public Block getPlant()
+	{
+		return null;
+	}
+
+	@Override
+	@Optional.Method(modid=MFRModuleBase.MOD_ID)
+	public HarvestType getHarvestType()
+	{
+		return HarvestType.Normal;
+	}
+
+	@Override
+	public boolean breakBlock()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean canBeHarvested(World world, Map<String, Boolean> harvesterSettings, int x, int y, int z)
+	{
+		return false;
+	}
+
+	@Override
+	public List<ItemStack> getDrops(World world, Random rand, Map<String, Boolean> harvesterSettings, int x, int y, int z)
+	{
+		return null;
+	}
+
+	@Override
+	public void preHarvest(World world, int x, int y, int z)
+	{
+	}
+
+	@Override
+	public void postHarvest(World world, int x, int y, int z)
+	{
+	}
+}
