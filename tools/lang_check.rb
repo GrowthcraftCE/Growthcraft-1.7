29cf568da0295aa6cb3854d6b3996c119d84b2ed
Corey Powell <mistdragon100@gmail.com>
2016-03-20 11:53:51 -0500

    tools: Updated Language tools

--- tools/lang_check.rb
+++ tools/lang_check.rb
@@ -1,30 +1,8 @@
 #!/usr/bin/env ruby
-require 'colorize'
-require 'active_support/core_ext/object/blank'
-require 'active_support/core_ext/string'
-
-class Lang < Struct.new(:filename, :table)
-end
-
-def parse_lang_file(contents, filename)
-  result = {}
-  contents.each_line.each_with_index do |line, i|
-    str = line.chomp
-    if str =~ /\A#/
-      # line is a comment
-    elsif str =~ /\A([\w\.\-_\|]+)\s*=(.*)/
-      result[$1] = $2
-    elsif str.blank?
-      # line is empty
-    else
-      warn "WARN: Odd line in \"#{filename}:#{i + 1}\" #{str}"
-    end
-  end
-  result
-end
+require_relative 'lib/language_file'
 
 def load_lang_file(filename)
-  parse_lang_file File.read(filename), File.expand_path(filename)
+  LangCheck::LanguageFile.load_file(filename)
 end
 
 # This utility script will cross-check all lang files against the en_US one to
@@ -35,33 +13,54 @@ Dir.chdir File.expand_path('../', __dir__) do
   Dir.chdir 'src/resources/assets' do
     modules = ARGV.presence || Dir.glob("./*").select { |d| File.directory?(d) }.map { |d| File.basename(d) }
 
+    languages = Dir.glob("**/*.lang").map { |fn| File.basename(fn, ".lang") }.uniq.sort
+
     modules.each do |m|
       Dir.glob("#{m}/lang/#{base_lang_name}.lang") do |en_filename|
         module_name = File.basename File.dirname File.dirname en_filename
         Dir.chdir File.dirname(en_filename) do
           langs = {}
-          Dir.glob("*.lang") do |filename|
-            langs[File.basename(filename, '.lang')] = Lang.new(File.expand_path(filename).gsub(/\A#{root_path}\//, ''), load_lang_file(filename))
+          languages.each do |lang_basename|
+            lang_filename = "#{lang_basename}.lang"
+            langs[lang_basename] = if File.exist?(lang_filename)
+              load_lang_file(lang_filename)
+            else
+              nil
+            end
           end
-          base_lang = langs.fetch(base_lang_name)
+          base_lang = langs[base_lang_name]
 
-          # ensure that anything in the base lang is in the other langs
-          langs.each_pair do |lang_name, lang|
-            puts "#{module_name} #{lang_name} (#{lang.filename})"
-            everything_ok = true
-            base_lang.table.each_pair do |name, _|
-              if lang.table.key?(name)
-                if lang.table[name].blank?
-                  STDERR.puts "\tERROR: Mapping for #{name} is empty!".light_red
-                  everything_ok = false
+          if base_lang
+            # ensure that anything in the base lang is in the other langs
+            langs.each_pair do |lang_name, lang|
+              if lang
+                puts "#{module_name} #{lang_name} (#{lang.filename})"
+                everything_ok = true
+                base_lang.table.each_pair do |name, _|
+                  if lang.table.key?(name)
+                    if lang.table[name].blank?
+                      STDERR.puts "\tERROR: Mapping for `#{name}` is empty!".light_red
+                      everything_ok = false
+                    end
+                  else
+                    STDERR.puts "\tERROR: No mapping for `#{name}`".light_red
+                    everything_ok = false
+                  end
+                end
+                extra_keys = lang.table.keys - base_lang.table.keys
+                if extra_keys.empty?
+                  STDOUT.puts "\tOK".light_green if everything_ok
+                else
+                  extra_keys.each do |key|
+                    STDERR
+                  end
                 end
               else
-                STDERR.puts "\tERROR: No mapping for #{name}".light_red
-                everything_ok = false
+                STDERR.puts "\tWARN: Translation `#{lang_name}` is missing for module `#{module_name}`"
               end
             end
-
-            STDOUT.puts "\tOK".light_green if everything_ok
+          else
+            STDERR.puts "\tCRITICAL-ERROR: `#{module_name}` has no `#{base_lang_name}` translation!".light_red
           end
         end
       end
