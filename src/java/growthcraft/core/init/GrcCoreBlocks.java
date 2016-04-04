95a2ab0c0c9cceac8a5cfe875be6eee049d7a6e0
Corey Powell <mistdragon100@gmail.com>
2016-04-04 11:39:04 -0500

    core: Added fence ropes for Et Futurum fences

--- src/java/growthcraft/core/init/GrcCoreBlocks.java
+++ src/java/growthcraft/core/init/GrcCoreBlocks.java
@@ -23,13 +23,20 @@
  */
 package growthcraft.core.init;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import growthcraft.core.common.block.BlockFenceRope;
 import growthcraft.core.common.block.BlockRope;
 import growthcraft.core.common.definition.BlockDefinition;
 import growthcraft.core.common.GrcModuleBase;
+import growthcraft.core.integration.minecraft.EnumMinecraftWoodType;
 import growthcraft.core.integration.NEI;
 import growthcraft.core.registry.FenceRopeRegistry;
 
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 
 public class GrcCoreBlocks extends GrcModuleBase
@@ -37,6 +44,7 @@ public class GrcCoreBlocks extends GrcModuleBase
 	public BlockDefinition ropeBlock;
 	public BlockDefinition fenceRope;
 	public BlockDefinition netherBrickFenceRope;
+	public Map<EnumMinecraftWoodType, BlockDefinition> etfuturumFenceRopes = new HashMap<EnumMinecraftWoodType, BlockDefinition>();
 
 	@Override
 	public void preInit()
@@ -52,18 +60,37 @@ public class GrcCoreBlocks extends GrcModuleBase
 	@Override
 	public void register()
 	{
-		//====================
-		// REGISTRIES
-		//====================
 		fenceRope.register("grc.fenceRope");
 		ropeBlock.register("grc.ropeBlock");
 		netherBrickFenceRope.register("grc.netherBrickFenceRope");
 
-		//====================
-		// ADDITIONAL PROPS.
-		//====================
 		Blocks.fire.setFireInfo(fenceRope.getBlock(), 5, 20);
+	}
 
+	private void initEtfuturum()
+	{
+		final String modId = "etfuturum";
+		if (Loader.isModLoaded(modId))
+		{
+			for (EnumMinecraftWoodType ty : EnumMinecraftWoodType.VALUES)
+			{
+				final Block block = GameRegistry.findBlock(modId, "fence_" + ty.name);
+				if (block != null)
+				{
+					final String basename = "grc.etfuturum_fence_rope_" + ty.name;
+					final BlockDefinition fp = new BlockDefinition(new BlockFenceRope(block, basename));
+					fp.register(basename);
+					Blocks.fire.setFireInfo(fp.getBlock(), 5, 20);
+					FenceRopeRegistry.instance().addEntry(block, fp.getBlock());
+				}
+			}
+		}
+	}
+
+	@Override
+	public void init()
+	{
+		initEtfuturum();
 		NEI.hideItem(fenceRope.asStack());
 		NEI.hideItem(netherBrickFenceRope.asStack());
 		NEI.hideItem(ropeBlock.asStack());
