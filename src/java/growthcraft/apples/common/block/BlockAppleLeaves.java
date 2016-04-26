1a031a78aa962a67411f32ae2fbe56ee1a36cf03
Joseph Caleb Jones <eyespot7b@aol.com>
2016-04-26 00:44:46 -0400

    apples: please jesus fucking compile on me
    attempt at adding oredict definitions, i get retarded ideas in the middle of the night

--- src/java/growthcraft/apples/common/block/BlockAppleLeaves.java
+++ src/java/growthcraft/apples/common/block/BlockAppleLeaves.java
@@ -22,6 +22,7 @@ import net.minecraft.world.ColorizerFoliage;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.IShearable;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class BlockAppleLeaves extends BlockLeavesBase implements IShearable, IGrowable
 {
@@ -420,4 +421,9 @@ public class BlockAppleLeaves extends BlockLeavesBase implements IShearable, IGr
 		ret.add(new ItemStack(Blocks.leaves, 1, world.getBlockMetadata(x, y, z) & 3));
 		return ret;
 	}
+	
+	@Override
+	public void init()
+	{
+		OreDictionary.registerOre("treeLeaves", appleLeaves.getItem());
 }
