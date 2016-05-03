5d803de72dbd19ad6d67fff68df12dd8ef1371aa
Joseph Caleb Jones <eyespot7b@aol.com>
2016-05-03 14:42:31 -0400

    Update GrcBeesItems.java

--- src/java/growthcraft/bees/init/GrcBeesItems.java
+++ src/java/growthcraft/bees/init/GrcBeesItems.java
@@ -33,7 +33,6 @@ import growthcraft.core.common.definition.ItemDefinition;
 import growthcraft.core.common.GrcModuleBase;
 
 import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 
