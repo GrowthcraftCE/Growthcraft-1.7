2b2ca3b24f5fa84d4d0a8ef1bee71366da26257b
Corey Powell <mistdragon100@gmail.com>
2016-05-07 18:40:19 -0500

    cellar: Added copyright header
    But it's MIT, what does it matter you ask?

--- src/java/growthcraft/cellar/common/item/ItemBoozeBottle.java
+++ src/java/growthcraft/cellar/common/item/ItemBoozeBottle.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015, 2016 IceDragon200
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package growthcraft.cellar.common.item;
 
 import java.util.List;
@@ -114,9 +137,6 @@ public class ItemBoozeBottle extends GrcItemFoodBase implements IFluidItem
 		}
 	}
 
-	/************
-	 * ON USE
-	 ************/
 	@Override
 	public ItemStack onEaten(ItemStack stack, World world, EntityPlayer player)
 	{
@@ -141,9 +161,6 @@ public class ItemBoozeBottle extends GrcItemFoodBase implements IFluidItem
 		return stack.stackSize <= 0 ? null : stack;
 	}
 
-	/************
-	 * TOOLTIP
-	 ************/
 	@Override
 	@SideOnly(Side.CLIENT)
 	@SuppressWarnings({"rawtypes", "unchecked"})
@@ -160,9 +177,6 @@ public class ItemBoozeBottle extends GrcItemFoodBase implements IFluidItem
 		}
 	}
 
-	/************
-	 * STUFF
-	 ************/
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void registerIcons(IIconRegister reg)
