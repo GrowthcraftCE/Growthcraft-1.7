d451bd47d778960417115def665400d92ad12e86
Corey Powell <mistdragon100@gmail.com>
2016-05-07 19:33:58 -0500

    api/core/effect: private constructor for EffectUtils
    Because it's a utility class duh

--- src/java/growthcraft/api/core/effect/EffectUtils.java
+++ src/java/growthcraft/api/core/effect/EffectUtils.java
@@ -29,6 +29,8 @@ import net.minecraft.potion.Potion;
 
 public class EffectUtils
 {
+	private EffectUtils() {}
+
 	public static EffectAddPotionEffect createAddPotionEffect(@Nonnull Potion p, int time, int level)
 	{
 		final IPotionEffectFactory factory = new SimplePotionEffectFactory(p.id, time, level);
