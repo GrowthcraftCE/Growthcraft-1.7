328a97a312b916605a2f3a765e24735b73a15ff1
Absinthe <Sunconure11@users.noreply.github.com>
2016-05-10 15:44:50 -0400

    Update GrcCoreItems.java

--- src/java/growthcraft/core/init/GrcCoreItems.java
+++ src/java/growthcraft/core/init/GrcCoreItems.java
@@ -68,7 +68,6 @@ public class GrcCoreItems extends GrcModuleBase
 		OreDictionary.registerOre("materialSalt", salt.getItem());
 		OreDictionary.registerOre("foodSalt", salt.getItem());
 		OreDictionary.registerOre("dustSalt", salt.getItem());
-		OreDictionary.registerOre("powderSalt", salt.getItem());
 		OreDictionary.registerOre("lumpSalt", salt.getItem());
 		OreDictionary.registerOre("bottleSalt", saltBottle.getItem());
 		OreDictionary.registerOre("bucketSalt", saltBucket.getItem());
