82647a8ae4c938db6ffd4b4a00117a001284b0c6
Corey Powell <mistdragon100@gmail.com>
2016-05-10 10:41:17 -0500

    core: Log when a IBucketEntry is added

--- src/java/growthcraft/core/eventhandler/EventHandlerBucketFill.java
+++ src/java/growthcraft/core/eventhandler/EventHandlerBucketFill.java
@@ -1,11 +1,33 @@
-/**
- * Gracefully taken from BuildCraft
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015, 2016 IceDragon200
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package growthcraft.core.eventhandler;
 
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.Nonnull;
+
+import growthcraft.core.GrowthCraftCore;
 
 import cpw.mods.fml.common.eventhandler.Event.Result;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -57,6 +79,11 @@ public class EventHandlerBucketFill
 		{
 			world.setBlockToAir(pos.blockX, pos.blockY, pos.blockZ);
 		}
+
+		public String toString()
+		{
+			return String.format("GenericBucketEntry{ block: {%s}, item_stack: {%s} }", block, itemStack);
+		}
 	}
 
 	private static EventHandlerBucketFill INSTANCE = new EventHandlerBucketFill();
@@ -70,6 +97,7 @@ public class EventHandlerBucketFill
 	public void addEntry(@Nonnull IBucketEntry entry)
 	{
 		buckets.add(entry);
+		GrowthCraftCore.getLogger().debug("Added new Bucket Entry {%s}", entry);
 	}
 
 	public EventHandlerBucketFill register(Block block, ItemStack stack)
