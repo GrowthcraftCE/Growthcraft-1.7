f75d939a94f14c0204280852edfefc7b57c3d2cc
Absinthe <Sunconure11@users.noreply.github.com>
2016-07-05 08:15:18 -0400

    Added oredictionary definitions for seeds

--- src/java/growthcraft/milk/init/GrcMilkItems.java
+++ src/java/growthcraft/milk/init/GrcMilkItems.java
@@ -229,6 +229,7 @@ public class GrcMilkItems extends GrcModuleBase
 		OreDictionary.registerOre("foodOffal", stomach.asStack());
 		OreDictionary.registerOre("materialStarterCulture", starterCulture.asStack());
 		OreDictionary.registerOre("materialCheeseCloth", cheeseCloth.asStack());
+		OreDictionary.registerOre("listAllseed", seedThistle.asStack());
 
 		OreDictionary.registerOre("foodMelon", Items.melon);
 		OreDictionary.registerOre("foodFruit", Items.melon);
