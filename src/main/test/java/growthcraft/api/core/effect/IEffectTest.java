f18f8a0977ec59f6cd9f4fc97e619d6438970fbc
Anonymous <Sunconure11@users.noreply.github.com>
2026-02-21 18:14:14 -0500

    more dick flattening

--- src/main/test/java/growthcraft/api/core/effect/IEffectTest.java
+++ src/main/test/java/growthcraft/api/core/effect/IEffectTest.java
@@ -4,13 +4,11 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class IEffectTest
-{
-	@Test
-	public void test_apply()
-	{
-		final TestEffect eff = new TestEffect();
-		eff.apply(null, null, null, null);
-		assertEquals(eff.touched, 1);
-	}
+public class IEffectTest {
+    @Test
+    public void test_apply() {
+        final TestEffect eff = new TestEffect();
+        eff.apply(null, null, null, null);
+        assertEquals(eff.touched, 1);
+    }
 }
--- src/main/test/java/growthcraft/api/core/effect/TestEffect.java
+++ src/main/test/java/growthcraft/api/core/effect/TestEffect.java
@@ -10,20 +10,20 @@ import net.minecraft.nbt.NBTTagCompound;
 /**
  * Test Effect, use this as apart of larger effects, and check if it was touched.
  */
-public class TestEffect implements IEffect
-{
-	public int touched;
+public class TestEffect implements IEffect {
+    public int touched;
 
-	public void apply(World world, Entity entity, Random random, Object data)
-	{
-		touched++;
-	}
+    public void apply(World world, Entity entity, Random random, Object data) {
+        touched++;
+    }
 
-	public void getDescription(List<String> list)
-	{
-		list.add("TestEffect");
-	}
+    public void getDescription(List<String> list) {
+        list.add("TestEffect");
+    }
 
-	public void readFromNBT(NBTTagCompound data, String name) {}
-	public void writeToNBT(NBTTagCompound data, String name) {}
+    public void readFromNBT(NBTTagCompound data, String name) {
+    }
+
+    public void writeToNBT(NBTTagCompound data, String name) {
+    }
 }
