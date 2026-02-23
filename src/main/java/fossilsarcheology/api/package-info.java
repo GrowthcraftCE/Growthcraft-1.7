64aebda3fdf5e244f4745cf0b9a40f67dc7b07ae
Anonymous <Sunconure11@users.noreply.github.com>
2026-02-23 15:43:13 -0500

    More API junk cleanup

--- src/main/java/fossilsarcheology/api/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author FossilsRevialTeam
- */
-@API(apiVersion = "7.3.1", owner = "fossil", provides = "Fossils and Archeology Revival API")
-package fossilsarcheology.api;
-
-import cpw.mods.fml.common.API;
