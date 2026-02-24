cdd44e655038a06776904aadd0ee8d50b16ffd38
Anonymous <Sunconure11@users.noreply.github.com>
2026-02-23 19:51:52 -0500

    Refactor, part 4

---
+++ src/main/test/java/growthcraft/api/core/effect/TestEffect.java
@@ -0,0 +1,29 @@
+package growthcraft.api.core.effect;
+
+import java.util.Random;
+import java.util.List;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
+import net.minecraft.nbt.NBTTagCompound;
+
+/**
+ * Test Effect, use this as apart of larger effects, and check if it was touched.
+ */
+public class TestEffect implements IEffect {
+    public int touched;
+
+    public void apply(World world, Entity entity, Random random, Object data) {
+        touched++;
+    }
+
+    public void getDescription(List<String> list) {
+        list.add("TestEffect");
+    }
+
+    public void readFromNBT(NBTTagCompound data, String name) {
+    }
+
+    public void writeToNBT(NBTTagCompound data, String name) {
+    }
+}
