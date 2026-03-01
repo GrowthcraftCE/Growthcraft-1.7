a423cb44ccfce2744ebca7eb4dd9df8d81de2f10
Anonymous <Sunconure11@users.noreply.github.com>
2026-03-01 13:38:35 -0500

    Beginning on Witchery beeboxes, albeit slowly

--- src/main/java/growthcraft/core/integration/witchery/EnumWitcheryWoodType.java
+++ src/main/java/growthcraft/core/integration/witchery/EnumWitcheryWoodType.java
@@ -1,7 +1,36 @@
 package growthcraft.core.integration.witchery;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import growthcraft.core.integration.EBXL.EBXLPlatform;
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+
+import java.util.Locale;
+
 public enum EnumWitcheryWoodType {
     ROWAN,
     ALDER,
     HAWTHORN;
+
+    public static final EnumWitcheryWoodType[] VALUES = values();
+    public final String name;
+    public final int meta;
+
+    EnumWitcheryWoodType() {
+        this.name = name().toLowerCase(Locale.ENGLISH);
+        this.meta = ordinal();
+    }
+
+    public ItemStack asPlanksItemStack(int size) {
+        final Block block = GameRegistry.findBlock(WitcheryPlatform.MOD_ID, "planks");
+        if (block != null) {
+            final ItemStack result = new ItemStack(block, size, meta);
+            return result;
+        }
+        return null;
+    }
+
+    public ItemStack asPlanksItemStack() {
+        return asPlanksItemStack(1);
+    }
 }
--- src/main/java/growthcraft/core/integration/witchery/WitcheryPlatform.java
+++ src/main/java/growthcraft/core/integration/witchery/WitcheryPlatform.java
@@ -1,4 +1,15 @@
 package growthcraft.core.integration.witchery;
 
+import cpw.mods.fml.common.Loader;
+
 public class WitcheryPlatform {
+
+    public static final String MOD_ID = "Witchery";
+
+    private WitcheryPlatform() {
+    }
+
+    public static boolean isLoaded() {
+        return Loader.isModLoaded(MOD_ID);
+    }
 }
