209835d8445274c1718a5d5f8c3ace30ecad68a4
Anonymous <Sunconure11@users.noreply.github.com>
2026-05-23 19:25:27 -0400

    Yeast stuff

--- src/main/java/growthcraft/cellar/GrowthCraftCellar.java
+++ src/main/java/growthcraft/cellar/GrowthCraftCellar.java
@@ -264,7 +264,9 @@ public class GrowthCraftCellar
 		OreDictionary.registerOre("yeastBayanus", EnumYeast.BAYANUS.asStack());
 		OreDictionary.registerOre("yeastEthereal", EnumYeast.ETHEREAL.asStack());
 		OreDictionary.registerOre("yeastOrigin", EnumYeast.ORIGIN.asStack());
-	}
+        OreDictionary.registerOre("yeastAlgol", EnumYeast.ALGOL.asStack());
+        OreDictionary.registerOre("yeastAntares", EnumYeast.ANTARES.asStack());
+    }
 
 	private void registerYeast()
 	{
@@ -273,6 +275,8 @@ public class GrowthCraftCellar
 		CellarRegistry.instance().yeast().addYeast(EnumYeast.BAYANUS.asStack());
 		CellarRegistry.instance().yeast().addYeast(EnumYeast.ETHEREAL.asStack());
 		CellarRegistry.instance().yeast().addYeast(EnumYeast.ORIGIN.asStack());
+        CellarRegistry.instance().yeast().addYeast(EnumYeast.ALGOL.asStack());
+        CellarRegistry.instance().yeast().addYeast(EnumYeast.ANTARES.asStack());
 	}
 
 	private void initVillageHandlers()
