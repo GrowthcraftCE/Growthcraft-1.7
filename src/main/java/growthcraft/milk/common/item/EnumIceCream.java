670b63c49db98570fd7b35dc07aff54b6ae7c0d3
Anonymous <Sunconure11@users.noreply.github.com>
2026-05-22 20:49:00 -0400

    Create some more ice cream flavors to represent some cross-mod compat

--- src/main/java/growthcraft/milk/common/item/EnumIceCream.java
+++ src/main/java/growthcraft/milk/common/item/EnumIceCream.java
@@ -35,7 +35,26 @@ public enum EnumIceCream implements IItemStackFactory
 	GRAPE("grape"),
 	APPLE("apple"),
 	HONEY("honey"),
-	WATERMELON("watermelon");
+	WATERMELON("watermelon"),
+    //Begin listing Et Futurum and Vanilla related content.
+    SWEET_BERRY("sweet_berry"),
+    GLOW_BERRY("glow_berry"),
+    //Yes, this actually exists.
+    KELP("kelp"),
+    ENCHANTED_APPLE("enchanted_apple"),
+    ENCHANTED_GOLDEN_APPLE("enchanted_golden_apple"),
+    CHORUS_FRUIT("chorus_fruit"),
+    PUMPKIN("pumpkin"),
+    //Begin listing Witchery content
+    ROWAN_BERRY("rowan_berry"),
+    //Begin listing Apple Milk Tea 2 content
+    YUZU("yuzu"),
+    TEA("tea"),
+    MINT("mint"),
+    CASSIS("cassis"),
+    CAMELLIA_FRUIT("camellia_fruit"),
+    //Begin listing Thaumcraft content
+    MANA_BEAN("mana_bean");
 
 	public static final EnumIceCream[] VALUES = values();
 
